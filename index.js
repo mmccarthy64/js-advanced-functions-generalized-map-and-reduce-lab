d34963d054390e9870b1c3638756b58d34fc3ea9
mmccarthy64 <mmccarthy64@hotmail.com>
2021-05-21 14:52:12 -0500

    commit tree 3fa9a1e393a0ccee2bec06f23e1b6d7d995d1633

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map(ray, callback){
+    const r  = [];
+
+    for (let i = 0; i < ray.length; i++){
+        const ele = ray[i];
+        r.push(callback(ele));
+    }
+    return r;
+}
+
+function reduce(ray, callback, start){
+    let total;
+    if(start){
+        total = start;
+        for(let i = 0; i < ray.length; i++){
+            total = callback(ray[i], total);
+        }
+        return total;
+    } else {
+        total = ray[0];
+        for(let i = 1; i < ray.length; i++){
+            total = callback(ray[i], total);
+        }
+        return total;
+    }
+    
+}
